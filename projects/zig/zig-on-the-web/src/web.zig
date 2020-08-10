6e88e5294d2b90595af557b741095ca343712cf7
Cass <casselenangulo@gmail.com>
2020-08-10 23:41:12 +0800

    CLEAN

--- projects/zig/zig-on-the-web/src/web.zig
+++
@@ -1,3 +0,0 @@
-// ------ language="Zig" file="projects/zig/zig-on-the-web/src/web.zig" project://article.md#422
-const window = @import("window.zig");
-// ------ end
--- projects/zig/zig-on-the-web/src/window.zig
+++
@@ -1,7 +0,0 @@
-// ------ language="Zig" file="projects/zig/zig-on-the-web/src/window.zig" project://article.md#435
-extern fn _alert(msg: [*]const u8, len: usize) void;
-
-pub fn alert(msg: []u8) void {
-  _alert(msg, msg.len);
-}
-// ------ end
